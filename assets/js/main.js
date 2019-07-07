fbf53eac1c9a54b542cf985cebb942ebb8beddd8
ncasanovas <nataliasolcasanovas@gmail.com>
2019-07-07 16:18:15 -0300

    Correcciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,8 +53,10 @@
 					hideOnSwipe: true,
 					resetScroll: true,
 					resetForms: true,
-					side: 'left'
+					side: 'left',
+					
 				});
+				
 				
 
 		// Fix: Remove transitions on WP<10 (poor/buggy performance).
@@ -64,22 +66,24 @@
 
 	});
 
+	
+
 })(jQuery);
 
 
 
 
-/* Effects */ 
+/* Effects Mouse */ 
 $("a").on("mouseover", function() {
     $(this).css("color", "grey");
 }).on("mouseout", function() {
-      $(this).css("color", "white");
+      $(this).css("color", "#000");
 });
-
+/*
 $("a>li").on("mouseover", function() {
     $(this).css("color", "grey");
 }).on("mouseout", function() {
-      $(this).css("color", "white");
+      $(this).css("color", "#000");
 });
 
 
@@ -87,10 +91,115 @@ $("a>i").on("mouseover", function() {
     $(this).css("color", "grey");
 }).on("mouseout", function() {
       $(this).css("color", "white");
+});*/
+
+
+
+/*Scroll*/
+
+/* Nav scroll*/
+
+
+
+$('#nav a[href^="#"]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top - 95
+			}, 1200);
+			return false;
+		}
+	}
 });
+/*
+$('a.uppercase.top').click(function() {
+	
+			 $("html, body").animate({    // catch the `html, body`
+			 scrollTop: $(this).offset().top - 100          // make their `scrollTop` property 0, to go at the top
+    }, 1200);
+			
+		
+	
+});*/
 
-
-
-$("a.uppercase").on("click", function() {
-    $("h3.uppercase").css("padding-top", "2.5em");
-});
+(function(document, history, location) {
+	var HISTORY_SUPPORT = !!(history && history.pushState);
+  
+	var anchorScrolls = {
+	  ANCHOR_REGEX: /^#[^ ]+$/,
+	  OFFSET_HEIGHT_PX: 80,
+  
+	  /**
+	   * Establish events, and fix initial scroll position if a hash is provided.
+	   */
+	  init: function() {
+		this.scrollToCurrent();
+		window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
+		document.body.addEventListener('click', this.delegateAnchors.bind(this));
+	  },
+  
+	  /**
+	   * Return the offset amount to deduct from the normal scroll position.
+	   * Modify as appropriate to allow for dynamic calculations
+	   */
+	  getFixedOffset: function() {
+		return this.OFFSET_HEIGHT_PX;
+	  },
+  
+	  /**
+	   * If the provided href is an anchor which resolves to an element on the
+	   * page, scroll to it.
+	   * @param  {String} href
+	   * @return {Boolean} - Was the href an anchor.
+	   */
+	  scrollIfAnchor: function(href, pushToHistory) {
+		var match, rect, anchorOffset;
+  
+		if(!this.ANCHOR_REGEX.test(href)) {
+		  return false;
+		}
+  
+		match = document.getElementById(href.slice(1));
+  
+		if(match) {
+		  rect = match.getBoundingClientRect();
+		  anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+		  window.scrollTo(window.pageXOffset, anchorOffset);
+  
+		  // Add the state to history as-per normal anchor links
+		  if(HISTORY_SUPPORT && pushToHistory) {
+			history.pushState({}, document.title, location.pathname + href);
+		  }
+		}
+  
+		return !!match;
+	  },
+  
+	  /**
+	   * Attempt to scroll to the current location's hash.
+	   */
+	  scrollToCurrent: function() {
+		this.scrollIfAnchor(window.location.hash);
+	  },
+  
+	  /**
+	   * If the click event's target was an anchor, fix the scroll position.
+	   */
+	  delegateAnchors: function(e) {
+		var elem = e.target;
+  
+		if(
+		  elem.nodeName === 'A' &&
+		  this.scrollIfAnchor(elem.getAttribute('href'), true)
+		) {
+		  e.preventDefault();
+		}
+	  }
+	};
+  
+	window.addEventListener(
+	  'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+	);
+  })(window.document, window.history, window.location);
